6f31e4624f901662147b4beb706252b739e0f4ad
clement <s10205095@connect.np.edu.sg>
2023-10-19 18:55:48 +0800

    sfsdfasdf

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import re
 import time
 import tqdm
 
+import multiprocessing
+
 import os
 import postprocessing
 import extract
 
 logger = logging.getLogger(__name__)
 
-_scanned_files = {}
-
-def main(args):
-    files = []
+def get_files_list(args):
+    excluded_files = []
 
     if args.postprocess_only == True:
         if 'all' not in args.languages:
@@ -23,95 +23,150 @@ def main(args):
         else:
             regex = f"(?i)\.({'|'.join(args.formats)})$"
 
+        if args.exclude_subtitles != None:
+            with open(args.exclude_subtitles) as f:
+                excluded_files = f.read().splitlines()
+
     else:
         regex = "(?i)\.(mkv|mp4|webm|ts|ogg)$"
 
-    for f in glob.iglob(args.path + '**/**', recursive=True):
-        if re.search(regex, f) != None:
-            files.append(f)
+        if args.exclude_videos != None:
+            with open(args.exclude_videos) as f:
+                excluded_files = f.read().splitlines()
+    
+    files = []
 
-    progress = tqdm.tqdm(files, desc=f'{datetime.datetime.now()} - Progress',
-                         unit='file') if args.progress_bar == 'on' else files
+    for f in glob.iglob(args.path + '**/**', recursive=True):
+        if re.search(regex, f) and f not in excluded_files and f not in files:
+            files.append(f)      
+
+    return files
+
+
+def run(threads,function,files):
+
+    run_output = []
+
+    def _run_callback(out):   
+        nonlocal run_output
+        run_output.extend(out)
+    
+    with multiprocessing.Pool(threads) as p:
+        p.map_async(
+            function,
+            files,
+            callback=_run_callback
+        )
+        p.close()
+        p.join()
+
+    return run_output
+
+def main(args):
+
+    files = get_files_list(args)
 
     if args.postprocess_only == True:
-        
-        for f in progress:
-            
-            if f in _scanned_files:
-                continue
-            else:
-                _scanned_files.setdefault(f)
-
-            try:
-                postprocessing.standardize(args.postprocessing, [f])
-            except KeyboardInterrupt:
-                exit(1)
-
-            except:
-                logger.critical("An error has occurred", exc_info=True)
-                continue
-
-
+        output = run(args.threads,postprocessing.standardize,files)
+    
     else:
         extractor = extract.SubtitleExtractor(
-            args.formats, args.languages, args.overwrite, args.unknown_language_as, not args.disable_bitmap_extraction)
+            args.formats, 
+            args.languages, 
+            args.overwrite, 
+            args.unknown_language_as, 
+            not args.disable_bitmap_extraction
+        )
 
-        for f in progress:
+        output = run(args.threads,extractor.extract,files)
 
-            if f in _scanned_files:
-                continue
-            else:
-                _scanned_files.setdefault(f)
+    print(output)
 
-            try:
-                subtitle_files = extractor.extract(f)
 
-                if args.postprocessing != None:
-                    postprocessing.standardize(
-                        args.postprocessing, subtitle_files)
 
-            except KeyboardInterrupt:
-                exit(1)
+    # if args.postprocess_only == True:
 
-            except:
-                logger.critical("An error has occurred", exc_info=True)
-                continue
-    
-    if args.exclude != None:
-        if args.exclude_mode == 'e+a':
-            with open(args.exclude, 'w') as f:
-                f.write("\n".join(_scanned_files.keys()))  
+    #     for f in progress:
+
+    #         if f in _scanned_files:
+    #             continue
+    #         else:
+    #             _scanned_files.setdefault(f)
+
+    #         try:
+    #             postprocessing.standardize(args.postprocessing, [f])
+    #         except KeyboardInterrupt:
+    #             exit(1)
+
+    #         except:
+    #             logger.critical("An error has occurred", exc_info=True)
+    #             continue
+
+    # else:
+    #     extractor = extract.SubtitleExtractor(
+    #         args.formats, args.languages, args.overwrite, args.unknown_language_as, not args.disable_bitmap_extraction)
+
+    #     for f in progress:
+
+    #         if f in _scanned_files:
+    #             continue
+    #         else:
+    #             _scanned_files.setdefault(f)
+
+    #         try:
+    #             subtitle_files = extractor.extract(f)
+
+    #             if args.postprocessing != None:
+    #                 postprocessing.standardize(
+    #                     args.postprocessing, subtitle_files)
+
+    #         except KeyboardInterrupt:
+    #             exit(1)
+
+    #         except:
+    #             logger.critical("An error has occurred", exc_info=True)
+    #             continue
+
+    # if args.exclude != None:
+    #     if args.exclude_mode == 'e+a':
+    #         with open(args.exclude, 'w') as f:
+    #             f.write("\n".join(_scanned_files.keys()))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--path',       help="Path to media file/folder", default="/media")
+        'path', help="path to media file/folder", default="/media")
     parser.add_argument(
-        '--formats', help="Subtitles formats to extract to", nargs='+', default=['srt'], choices=extract.SUPPORTED_FORMATS)
+        '--formats', help="output subtitles formats", nargs='+', default=['srt'], choices=extract.SUPPORTED_FORMATS)
     parser.add_argument(
-        '--languages', help="Select subtitle languages stream to extract from, use 'all' to extract all languages", nargs='+', default=['eng'])
+        '--languages', help="extract subtitle for selected languages, use 'all' to extract all languages", nargs='+', default=['all'])
     parser.add_argument(
-        '--unknown_language_as', help="Treat unknown language as", type=str, default=None)
+        '--unknown_language_as', help="treat unknown language as", type=str, default=None)
     parser.add_argument(
-        '--overwrite', help="Overwrite existing subtitle file", action='store_true')
+        '--overwrite', help="overwrite existing subtitle file", action='store_true')
     parser.add_argument(
-        '--disable_bitmap_extraction', help="Disable bitmap subtitle extraction via OCR", action='store_true')
+        '--disable_bitmap_extraction', help="disable extraction for bitmap based subtitle extraction via OCR", action='store_true')
     parser.add_argument(
-        '--postprocess_only', help="Only do conduct post processing", action='store_true')
+        '--postprocess_only', help="only do conduct post processing", action='store_true')
     parser.add_argument(
-        '--postprocessing', help="Path to postprocessing config file", type=str, default=None)
+        '--postprocessing', help="path to postprocessing config file", type=str, default=None)
     parser.add_argument(
-        '--scan_interval', help="Interval to scan folder in mins (set 0 to disable and exit upon completion) ", type=int, default=0)
+        '--scan_interval', help="interval to monitor and scan folder in mins (set 0 to disable and exit upon completion) ", type=int, default=0)
     parser.add_argument(
-        "--log_level", help="Setting logging level", default='INFO')
+        "--log_level", help="setting logging level", default='WARNING')
     parser.add_argument(
-        "--log_file", help="Path to log file", default=None)
+        "--log_file", help="path to log file", default=None)
     parser.add_argument(
-        "--progress_bar", help="Enable progress bar", type=str, default='on', choices=['on', 'off'])
+        "--progress_bar", help="enable progress bar", type=str, default='on', choices=['on', 'off'])
     parser.add_argument(
-        "--exclude", help="path to a newline separated file with paths to exclude", type=str, default=None)
+        "--exclude_videos", help="path to a newline separated file with paths to video files to exclude", type=str, default=None)
     parser.add_argument(
-        "--exclude_mode", help="set file exclusion behavior, e = exclude only, e+a = exclude and append file that are already extract", type=str, default='e', choices=['e','e+a'])
+        "--exclude_subtitles", help="path to a newline separated file with paths to subtitles files to exclude", type=str, default=None)
+    parser.add_argument(
+        "--exclude_mode", help="set file exclusion behavior, e = exclude only, e+a = exclude and append new extracted file", type=str, default='e', choices=['e', 'e+a'])
+    parser.add_argument(
+        '--threads', help="treat unknown language as", type=int, default=4)
 
     args = parser.parse_args()
 
@@ -119,11 +174,6 @@ if __name__ == '__main__':
         format='%(asctime)s - %(name)s - %(funcName)s() - %(levelname)s - %(message)s', level=args.log_level)
     if args.log_file != None:
         logging.getLogger().addHandler(logging.FileHandler(args.log_file))
-
-    if args.exclude != None and os.path.exists(args.exclude):
-        with open(args.exclude) as f:
-            for p in f.read().splitlines():
-                _scanned_files.setdefault(p)
 
 
     main(args)
--- postprocessing.py
+++ postprocessing.py
@@ -9,24 +9,24 @@ logger = logging.getLogger(__name__)
 
 class BaseFormatter:
 
-    def __init__(self,config: dict) -> None:
+    def __init__(self, config: dict) -> None:
 
         self.config = config
-        self.general = config.get('general',{})
-        self.regex_replace = config.get("replace",[])
-        
-        self.save_kwargs = config.get('save',{})
-        
-    def format_general(self, ssafile:pysubs2.SSAFile):
+        self.general = config.get('general', {})
+        self.regex_replace = config.get("replace", [])
+
+        self.save_kwargs = config.get('save', {})
+
+    def format_general(self, ssafile: pysubs2.SSAFile):
 
         ssafile = copy.deepcopy(ssafile)
-        
-        if self.general.get('remove_miscellaneous_events',False):
+
+        if self.general.get('remove_miscellaneous_events', False):
             ssafile.remove_miscellaneous_events()
 
-        rm_drawing = self.general.get('remove_drawings',False)
-        rm_dup = self.general.get("remove_duplicate_lines",False)
-        rm_comments = self.general.get('remove_comments',False)
+        rm_drawing = self.general.get('remove_drawings', False)
+        rm_dup = self.general.get("remove_duplicate_lines", False)
+        rm_comments = self.general.get('remove_comments', False)
 
         prev_event = pysubs2.SSAEvent()
 
@@ -35,99 +35,104 @@ class BaseFormatter:
             if rm_comments and event.is_comment:
                 logger.debug(f"Removing comment line:{i}")
                 ssafile.events.pop(i)
-            
+
             if rm_drawing and event.is_drawing:
                 logger.debug(f"Removing drawing line:{i}")
                 ssafile.events.pop(i)
 
-            if rm_dup:    
+            if rm_dup:
                 if event.start == prev_event.start and event.end == prev_event.end and event.plaintext == prev_event.plaintext:
                     logger.debug(f"Removing duplicate line:{i}")
                     ssafile.events.pop(i)
-            
+
             for r in self.regex_replace:
                 old = event.text
-                event.text = re.sub(r["regex"],r["replacement"],event.text)
+                event.text = re.sub(r["regex"], r["replacement"], event.text)
 
                 if old != event.text:
-                    logger.debug(f"Performing regex ({r['regex']}) replacement for line:{i}")
+                    logger.debug(
+                        f"Performing regex ({r['regex']}) replacement for line:{i}")
 
             prev_event = event
-        
+
         return ssafile
 
-    def format(self,path, save = False):
+    def format(self, path, save=False):
         ssafile = pysubs2.load(path)
-        
+
         ssafile = self.format_general(ssafile)
 
         if save == True:
-            ssafile.save(path,**self.save_kwargs)
-        
+            ssafile.save(path, **self.save_kwargs)
+
         return ssafile
 
 
 class SSAFormatter(BaseFormatter):
-    
+
     def __init__(self, config: dict) -> None:
         super().__init__(config)
 
-        self.info = config.get("info",{})
-        self.styles = config.get("styles",[])
-    
-    def update_info(self,ssafile:pysubs2.SSAFile):
+        self.info = config.get("info", {})
+        self.styles = config.get("styles", [])
+
+    def update_info(self, ssafile: pysubs2.SSAFile):
 
         ssafile = copy.deepcopy(ssafile)
 
-        remove = self.info.get("remove",[])
-        update = self.info.get("update",{})
+        remove = self.info.get("remove", [])
+        update = self.info.get("update", {})
 
         for key in remove:
-            ssafile.info.pop(key,None)
-        
+            ssafile.info.pop(key, None)
+
         ssafile.info.update(update)
 
         return ssafile
-    
-    def format_ssa_styles(self,ssafile):
-        
+
+    def format_ssa_styles(self, ssafile):
+
         ssafile = copy.deepcopy(ssafile)
 
-        ssafile.info.setdefault("PlayResX",'1920')
-        ssafile.info.setdefault("PlayResY",'1080')
+        ssafile.info.setdefault("PlayResX", '1920')
+        ssafile.info.setdefault("PlayResY", '1080')
 
         for key in ssafile.styles.keys():
 
             for sty in self.styles:
                 sty_copy = sty.copy()
-                
+
                 match_regex = sty_copy.pop("regex")
                 mode = sty_copy.pop("mode")
 
                 if "fontsize" in sty_copy:
-                    sty_copy["fontsize"] = round(sty_copy["fontsize"] / 1080 * float(ssafile.info["PlayResY"]), 1)
-                
+                    sty_copy["fontsize"] = round(
+                        sty_copy["fontsize"] / 1080 * float(ssafile.info["PlayResY"]), 1)
+
                 if "marginv" in sty_copy:
-                    sty_copy["marginv"] = int(round(sty_copy["marginv"] / 1080 * float(ssafile.info["PlayResY"])))
+                    sty_copy["marginv"] = int(
+                        round(sty_copy["marginv"] / 1080 * float(ssafile.info["PlayResY"])))
 
                 if "marginl" in sty_copy:
-                    sty_copy["marginl"] = int(round(sty_copy["marginl"] / 1920 * float(ssafile.info["PlayResX"])))
-                    
-                if "marginr" in sty_copy: 
-                    sty_copy["marginr"] = int(round(sty_copy["marginr"] / 1920 * float(ssafile.info["PlayResX"])))
+                    sty_copy["marginl"] = int(
+                        round(sty_copy["marginl"] / 1920 * float(ssafile.info["PlayResX"])))
 
-                if re.fullmatch(match_regex,str(key)) != None:
+                if "marginr" in sty_copy:
+                    sty_copy["marginr"] = int(
+                        round(sty_copy["marginr"] / 1920 * float(ssafile.info["PlayResX"])))
+
+                if re.fullmatch(match_regex, str(key)) != None:
 
                     if mode == 'replace':
                         logger.debug(f"Replacing style: {key}")
                         ssafile.styles[key] = pysubs2.SSAStyle(**sty_copy)
-                    
+
                     elif mode == 'update':
                         logger.debug(f"Updating style: {key}")
                         ssafile.styles[key].__dict__.update(sty_copy)
                     else:
                         logger.critical(f"'{mode}' mode is not Supported")
-        
+
         return ssafile
 
     def format(self, path, save=False):
@@ -138,39 +143,40 @@ class SSAFormatter(BaseFormatter):
         ssafile = self.format_ssa_styles(ssafile)
 
         if save == True:
-            ssafile.save(path,**self.save_kwargs)
+            ssafile.save(path, **self.save_kwargs)
 
         return ssafile
 
+
 class SRTFormatter(BaseFormatter):
-        
+
     def __init__(self, config: dict) -> None:
         super().__init__(config)
 
 
-def standardize(config_path,files):
+def standardize(config_path: str, path: str) -> str:
     with open(config_path) as cfg_file:
         config = yaml.full_load(cfg_file.read())
 
-    for path in files:
-        
-        try:
-            if str(path).endswith(".ass"):
-                logger.info(f"[PostProcessing] Formatting ass subtitle: {path}")
-                formatter = SSAFormatter(config['ass'])
-                formatter.format(path,save=True)
-            
-            elif str(path).endswith(".srt") or str(path).endswith(".vtt"):
-                logger.info(f"[PostProcessing] Formatting srt subtitle: {path}")
-                formatter = SRTFormatter(config['srt'])
-                formatter.format(path,save=True)
-            
-            else:
-                logger.warning("[PostProcessing] Unsupported format")
-        
-        except (KeyboardInterrupt,SystemExit) as e:
-            raise e
+    try:
+        if str(path).endswith(".ass"):
+            logger.info(f"[PostProcessing] Formatting ass subtitle: {path}")
+            formatter = SSAFormatter(config['ass'])
+            formatter.format(path, save=True)
 
-        except:
-            logger.critical("[Postprocessing] An error has occurred", exc_info=True)
-            continue
+        elif str(path).endswith(".srt") or str(path).endswith(".vtt"):
+            logger.info(f"[PostProcessing] Formatting srt subtitle: {path}")
+            formatter = SRTFormatter(config['srt'])
+            formatter.format(path, save=True)
+
+        else:
+            logger.warning("[PostProcessing] Unsupported format")
+
+    except (KeyboardInterrupt, SystemExit) as e:
+        raise e
+
+    except:
+        logger.critical(
+            "[Postprocessing] An error has occurred", exc_info=True)
+
+    return path
